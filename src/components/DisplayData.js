d68942254fff5b76de71a90138fd5256e165aca3
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-16 13:45:54 +0100

    introduced ParentComponent.js, added DisplayData.js

---
+++ src/components/DisplayData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DisplayData({ firstName, lastName }) {
+  return (
+    <>
+      <h1>{firstName}</h1>
+      <h1>{lastName}</h1>
+    </>
+  );
+}
+
+export default DisplayData;
